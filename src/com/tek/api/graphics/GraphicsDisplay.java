a4123b2f43a3935dcd4e1b739ec5f56640a841bc
RedstoneTek <xxredstonetekxx@gmail.com>
2017-01-29 09:31:36 -0500

    Initial Commit

---
+++ src/com/tek/api/graphics/GraphicsDisplay.java
@@ -0,0 +1,30 @@
+package com.tek.api.graphics;
+
+import java.awt.Canvas;
+
+import javax.swing.JFrame;
+
+public class GraphicsDisplay extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+	
+	Canvas canvas;
+
+	public GraphicsDisplay(String title, int width, int height) {
+		setTitle(title);
+		setSize(width,height);
+		
+		getContentPane().setLayout(null);
+		
+		canvas = new Canvas();
+		canvas.setBounds(0, 0, width, height);
+		getContentPane().add(canvas);
+		
+		setResizable(false);
+		setVisible(true);
+	}
+	
+	public Canvas getCanvas(){
+		return canvas;
+	}
+}
